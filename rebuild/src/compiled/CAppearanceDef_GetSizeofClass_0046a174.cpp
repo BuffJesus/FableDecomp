43e6e789b99e21defee678612b5d9c007a3e764e
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-07-23 16:47:53 -0600

    Stage batch17 and auto-land 5 tiny class-size stubs

---
+++ rebuild/src/compiled/CAppearanceDef_GetSizeofClass_0046a174.cpp
@@ -0,0 +1,5 @@
+#pragma optimize("s",on)
+int __fastcall CAppearanceDef_GetSizeofClass()
+{
+    return 120;
+}
---
+++ rebuild/src/compiled/CQuickAccessItemButtonGuiDef_GetSizeofClass_0046ee66.cpp
@@ -0,0 +1,5 @@
+#pragma optimize("s",on)
+int __fastcall CQuickAccessItemButtonGuiDef_GetSizeofClass()
+{
+    return 80;
+}
