a45e7f0eb157d3c86affcf84603d953e58909fed
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-09-07 21:17:42 -0600

    Retype expression prevention pair

--- rebuild/src/compiled/00/84/CCreatureActionPerformExpressionExtended_PreventExpression_00842c50.cpp
+++ rebuild/src/compiled/00/84/CCreatureActionPerformExpressionExtended_PreventExpression_00842c50.cpp
@@ -1,11 +1,13 @@
+#include "engine/CCreatureAction_PerformExpressionExtended.h"
 #include "rebuild_abi.h"
-// CCreatureAction_PerformExpressionExtended::PreventExpression @ 0x00842c50
-// mov ecx,[ecx+0xa8]; push STR; add ecx,0x78; call ctor; ret
-// Assign a fixed string into a CExprField subobject at (this->fa8)+0x78 (thiscall).
+
 extern const char kExprName_00842c50[];
 struct CExprField { void Assign(const char *name); };
-struct CCreatureAction_PerformExpressionExtended { char pad00[0xa8]; char *m_holder; void PreventExpression(); };
-void CCreatureAction_PerformExpressionExtended::PreventExpression()
+struct CCreatureAction_PerformExpressionExtended_Methods : CCreatureAction_PerformExpressionExtended {
+    void PreventExpression();
+};
+
+void CCreatureAction_PerformExpressionExtended_Methods::PreventExpression()
 {
-    ((CExprField*)(m_holder + 0x78))->Assign(kExprName_00842c50);
+    ((CExprField*)((char*)p + 0x78))->Assign(kExprName_00842c50);
 }
--- rebuild/src/compiled/00/84/CCreatureActionPerformExpression_PreventExpression_00842c00.cpp
+++ rebuild/src/compiled/00/84/CCreatureActionPerformExpression_PreventExpression_00842c00.cpp
@@ -1,11 +1,13 @@
+#include "engine/CCreatureAction_PerformExpression.h"
 #include "rebuild_abi.h"
-// CCreatureAction_PerformExpression::PreventExpression @ 0x00842c00
-// mov ecx,[ecx+0xa8]; push STR; add ecx,0x78; call ctor; ret
-// Assign a fixed string into a CExprField subobject at (this->fa8)+0x78 (thiscall).
+
 extern const char kExprName_00842c00[];
 struct CExprField { void Assign(const char *name); };
-struct CCreatureAction_PerformExpression { char pad00[0xa8]; char *m_holder; void PreventExpression(); };
-void CCreatureAction_PerformExpression::PreventExpression()
+struct CCreatureAction_PerformExpression_Methods : CCreatureAction_PerformExpression {
+    void PreventExpression();
+};
+
+void CCreatureAction_PerformExpression_Methods::PreventExpression()
 {
-    ((CExprField*)(m_holder + 0x78))->Assign(kExprName_00842c00);
+    ((CExprField*)((char*)p + 0x78))->Assign(kExprName_00842c00);
 }
